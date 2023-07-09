2a349acf41984a97ca08e5dc5dadf2879abcf4d6
Raahim Idrees <raahim.idrees@gmail.com>
2023-07-08 22:08:55 -0700

    paginating requests in batches

--- main.py
+++ main.py
@@ -15,7 +15,13 @@ users_saved_tracks = results['items']
 while results['next']:
     results = sp.next(results)
     users_saved_tracks.extend(results['items'])
-    
+
+# batches for adding tracks
+batch_size = 100
+
+#split tracks into batches
+track_batches = [users_saved_tracks[i:i+batch_size] for i in range(0, len(users_saved_tracks), batch_size)]
+
 #create playlist
 playlist_name = 'My Liked Songs - Public'
 
@@ -24,12 +30,18 @@ existing_playlist = next((playlist for playlist in existing_playlists['items'] i
 
 if existing_playlist: 
     playlist_id = existing_playlist['id']
-    sp.playlist_replace_items(playlist_id, [track['track']['id'] for track in users_saved_tracks])
-    print('Playlist updated successfully!')
+    existing_tracks = sp.playlist_items(playlist_id, fields='items(track.id,total)')
+    existing_track_ids = [track['track']['id'] for track in existing_tracks['items']]
 
 else: 
     #create a new playlist
     playlist_description = 'All songs I have ever saved for the public'
     playlist = sp.user_playlist_create(sp.current_user()['id'], playlist_name, public=True, description=playlist_description)
-    sp.user_playlist_add_tracks(ssfp.current_user()['id'], playlist['id'], [track['track']['id'] for track in users_saved_tracks])
-    print("Created playlist succesully")
+    playlist_id = playlist['id']
+    existing_track_ids = []
+
+# Add tracks to the playlist in batches
+for batch in track_batches:
+    track_ids = [track['track']['id'] for track in batch if track['track']['id'] not in existing_track_ids]
+    if track_ids:
+        sp.playlist_add_items(playlist_id, track_ids)
